841541edc62d602f350c6ee15f29e2485278632c
AndreasFahey <34457372+AndreasFahey@users.noreply.github.com>
2019-03-26 20:55:03 +0000

    Second Commit
    This is where i begin coding. Here i coded a loop for character input. Comments will be added later or at the end in final commit.

--- GTProject/project2019.py
+++ GTProject/project2019.py
@@ -4,6 +4,34 @@
 #Andreas Fahey
 #G00346830
 
+def shunt(infix):
+    """SYA To Convert Regular Expressions from infix to postfix"""
 
+    specials = {
+        "*": 50,
+        ".": 40,
+        "|": 30
+    }
 
+    postfix = ""
+    stack = ""
+
+    for i in infix:
+        if i == "(":
+            stack += i
+        elif i == ")":
+            while stack[-1] != "(":
+                postfix, stack = postfix + stack[-1], stack[:-1]
+            stack = stack[:-1]
+        elif i in specials:
+            while stack and specials.get(i, 0) <= specials.get(stack[-1], 0):
+                postfix, stack = postfix + stack[-1], stack[:-1]
+            stack = stack + i
+        else:
+            postfix = postfix + i
+
+    while stack:
+        postfix, stack = postfix + stack[-1], stack[:-1]
+    
+    return postfix 
     
